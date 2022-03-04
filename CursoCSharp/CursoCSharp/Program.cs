fca215d85cb95be305096ef65d600faecbb2e5e5
kevin alexsander <kevin13348@gmail.com>
2022-03-04 10:52:12 -0300

    Operadores Aritmeticos

--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -16,6 +16,7 @@ namespace CursoCSharp {
                 {"Lendo Dados - Fundamentos", LendoDados.Executar },
                 {"Formatando Números - Fundamentos", FormatandoNumeros.Executar },
                 {"Conversões - Fundamentos", Conversoes.Executar },
+                {"Operadores Aritmeticos - Fundamentos", OperadoresAritmeticos.Executar },
             });
 
             central.SelecionarEExecutar();
